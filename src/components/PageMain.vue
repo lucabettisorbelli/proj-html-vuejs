9fe5f6ee18276907d5d28debaeda3b1e7d86b977
luca betti sorbelli <luca betti sorbelli>
2023-06-01 12:00:18 +0200

    fix MainBottom

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,4 +1,175 @@
 <script>
-
+export default {
+    name: 'PageMain',
+    data() {
+        return {
+            images: [
+                {
+                    src: 'logoTrimcut.png',
+                    title: 'Trim & Cut',
+                    subscription: 'Avada Barbers are exsperts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+                },
+                {
+                    src: 'logoWashdry.png',
+                    title: 'Wash & Dry',
+                    subscription: 'Take a seat in your leather chairs, lean back and let us lather you a fresh head in a luxurious fashion.'
+                },
+                {
+                    src: 'logoBeardtrim.png',
+                    title: 'Beard Tidy',
+                    subscription: 'Tame the tangles and untidy facial hairs like a gentleman with our Beard Tidy services from Avada Barbers.'
+                }
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../assets/${name}`, import.meta.url).href
+        }
+    }
+}
 </script>
-<template></template>
+
+
+<template>
+    <main>
+        <div class="containerMainTop">
+            <div class="containerBoxed">
+                <div class="boxTitle">
+                    <div class="subtitle">
+                        <h6>MENS GROOMING</h6>
+                    </div>
+                    <div class="title">
+                        <h1>Services</h1>
+                    </div>
+                </div>
+                <div class="elementsBarber">
+                    <div class="element" v-for="element in images">
+                        <div class="images">
+                            <img :src="getImageUrl(element.src)" alt="">
+                        </div>
+                        <div class="elementTitle">{{ element.title }}</div>
+                        <small class="elementSubtitle">{{ element.subscription }}</small>
+                    </div>
+                </div>
+                <div class="button">
+                    <button>READ ABOUT OUR SERVICES</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="containerMainBottom">
+            <div class="backgroundImage">
+                <img src="../assets/triangle.svg" alt="">
+            </div>
+            <div class="containerBg">
+                <div class="containerboxedLarge">
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+.containerMainTop {
+    height: 640px;
+}
+
+.containerBoxed {
+    height: 100%;
+    max-width: 1150px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+
+
+    .boxTitle {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .subtitle {
+            color: #be9359;
+        }
+
+        .title {
+            color: #b0b0b0;
+            padding-top: 10px;
+        }
+    }
+}
+
+.elementsBarber {
+    display: flex;
+    justify-content: space-between;
+    height: 300px;
+    width: 100%;
+    margin-top: 50px;
+
+    .element {
+        width: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        .images {
+            width: 80px;
+            height: 120px;
+
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        .elementTitle {
+            color: #be9359;
+        }
+
+        .elementSubtitle {
+            color: rgba(27, 24, 24, 0.3);
+            text-align: center;
+        }
+    }
+}
+
+.button {
+    margin-top: 50px;
+
+    button {
+        border: 1px solid #be9359;
+        color: #be9359;
+        padding: 10px 20px;
+        margin-top: 1.5rem;
+        background-color: transparent;
+    }
+}
+
+.containerMainBottom {
+    height: 1000px;
+
+    .backgroundImage {
+        filter: brightness(0);
+        height: 100px;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    .containerBg {
+        height: 100%;
+        background-color: black;
+    }
+
+    .containerboxedLarge {
+        height: 100%;
+        max-width: 1520px;
+        margin: 0 auto;
+    }
+}
+</style>
